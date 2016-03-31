3f4c0c688376a91f609719e06abd96057c51fd66
Daven Farnham <daven.farnham@gmail.com>
2016-03-31 11:56:20 -0400

    parts 1 and 2 of pset5 done

---
+++ 5/section/blank_functor.ml
@@ -0,0 +1,86 @@
+open Core.Std
+
+(* a simplified, shortened verson of SET *)
+module type SET =
+sig
+  type elt  (* type of elements in the set *)
+  type set  (* abstract type for the set *)
+
+  val empty : set
+
+  val is_empty : set -> bool
+
+  val insert : elt -> set -> set
+
+  (* same as insert x empty *)
+  val singleton : elt -> set
+
+  val union : set -> set -> set
+
+  (* returns true iff the element is in the set *)
+  val member : set -> elt -> bool
+  
+  (* chooses some member from the set, removes it
+   * and returns that element plus the new set.
+   * If the set is empty, returns None. *)
+  val choose : set -> (elt * set) option
+end
+
+module type COMPARABLE =
+sig
+  type t
+  val compare : t -> t -> Ordering.t
+end
+        
+module IntCompare  =
+struct
+  type t = int
+            
+  let compare t1 t2 = if t1 > t2 then Greater else if t2 > 1 then Less else Equal;;       
+end
+
+module Queue =
+struct
+  type 'a t = 'a list
+
+  let emp : 'a t = []
+
+  let put (q : 'a t) (v : 'a) : 'a t =
+    q @ [v]
+  ;;
+
+  let take (q : 'a t) : ('a * 'a t) option =
+    match q with
+    | [] -> None
+    | hd :: tl -> Some (hd, tl)
+  ;;
+end
+
+module QueueSet (C : COMPARABLE) : (SET with type elt = C.t) = 
+struct
+
+  type elt = C.t
+  type set = C.t Queue.t
+
+  let empty = Queue.emp
+
+  let is_empty s = (s = Queue.emp)
+
+  let insert e s = Queue.put s e
+
+  let singleton e = Queue.put Queue.emp  e 
+
+  let rec union s1 s2 = match Queue.take s1 with
+                        | None -> s2 
+		        | Some (e, s') -> union s' (Queue.put s2 e);;
+
+  let 
+end
+
+(* module Q = QueueSet(IntCompare);; *)
+
+(* Q.choose (Q.union (Q.singleton 4) (Q.insert 5 Q.empty)) *)
+
+(* match (Q.choose (Q.union (Q.singleton 4) (Q.insert 5 Q.empty))) with
+   | None -> None
+   | Some (e, s) -> Q.choose s;; *)
---
+++ 5/section/opening.ml
@@ -0,0 +1,10 @@
+open List
+
+module Test = 
+struct 
+
+  type t = int list
+
+  let length = 5
+
+end
