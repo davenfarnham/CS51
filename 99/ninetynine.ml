6c69e498de07cfc555f2ec1c671799270ce46b08
Daven Farnham <daven.farnham@gmail.com>
2016-05-27 00:38:34 -0400

    finished konami interview question

--- 99/ninetynine.ml
+++ 99/ninetynine.ml
@@ -156,3 +156,70 @@ let encode l =
 
 assert(encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"] = 
 	      [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")])
+
+
+(* street fighter interview question *)
+type keystroke = Up | Down | Left | Right | A | B
+type tree = Leaf | Empty | Branch of (string * tree * tree * tree * tree * tree * tree) 
+
+(* empty tree refs *)
+let t = ref (Branch("", Leaf, Leaf, Leaf, Leaf, Leaf, Leaf)) 
+let full = ref Empty
+
+let check t = 
+  match t with 
+  | Leaf -> (Branch("", Leaf, Leaf, Leaf, Leaf, Leaf, Leaf))
+  | _ -> t
+
+(* essentially a trie implemented using a tree *)
+let register (l : keystroke list) (name : string) : unit = 
+    let rec insert l' t' = 
+      (match t' with
+       | Branch(s, u, d, l, r, a, b) -> (match l' with
+      					 | [] -> Branch(name, u, d, l, r, a, b)
+					 | Up :: tl -> Branch(s, insert tl (check u), d, l, r, a, b)
+					 | Down :: tl -> Branch(s, u, insert tl (check d), l, r, a, b)
+					 | Left :: tl -> Branch(s, u, d, insert tl (check l), r, a, b)
+					 | Right :: tl -> Branch(s, u, d, l, insert tl (check r), a, b)
+					 | A :: tl -> Branch(s, u, d, l, r, insert tl (check a), b)
+					 | B :: tl -> Branch(s, u, d, l, r, a, insert tl (check b))) 
+       | _ -> raise Error) in
+  (t := (insert l !t))
+;;
+
+let is_name tr = 
+  match tr with
+  | Branch (s, _, _, _, _, _, _) -> if not (s = "") then (print_string (s ^ "\n"); (t := !full); true) else (t := tr; false)
+  | _ -> ((t := !full); false)
+
+let on_key (k: keystroke) : bool = 
+  match !t with
+  | Leaf | Empty -> false
+  | Branch(_, u, d, l, r, a, b) -> (match k with
+				    | Up -> is_name u
+				    | Down -> is_name d
+				    | Left -> is_name l
+				    | Right -> is_name r
+				    | A -> is_name a
+				    | B -> is_name b)
+
+let play l = 
+  (full := !t);
+  let rec loop l' = 
+    (match !t with
+     | Branch(_, Leaf, Leaf, Leaf, Leaf, Leaf, Leaf) -> ((t := !full); loop l')
+     | _ -> (match l' with
+             | [] -> ()
+      	     | hd :: tl -> ignore (on_key hd); loop tl)) in
+  loop l
+
+
+let _ = 
+    (register [Up; Down; Up; Down] "Punch"); 
+    (register [Left; Right; Left; Up] "Kick");
+    (register [Up; Up; Down; Down; Left; Right; Left; Right; B; A] "Konami");
+    play [Up; Down; Up; Down; Left; Right; Left; Up]; 								(* Punch; Kick *)
+    play [Up; Down; Up; Down; A; Left; Right; A; Left; Up];							(* Punch; Kick is interrupted *)
+    play [Up; Down; Up; Down; Up; Up; Down; Down; Left; Right; Left; Right; B; A; Left; Right; Left; Up]	(* Punch; Konami; Kick *)
+
+
